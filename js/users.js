eeb5b11676d3db0ce12653171f16401b8be8856e
ntruter42 <ntruter42@gmail.com>
2023-05-18 11:08:01 +0200

    Update index.html, users.js - updated template to loop through list of dataset for multiple users

--- js/users.js
+++ js/users.js
@@ -6,12 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
 	var userTemplate = Handlebars.compile(templateSource);
 
 	// pass the data into the template & get the HTML back
-	var userDataHTML = userTemplate({
-		username: 'lindani',
-		firstName: 'Lindani',
-		lastName: 'Pani',
-		email: 'lindani@pani.com'
-	});
+	var userData = {
+		users: [
+			{ username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
+			{ username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
+			{ username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
+		]
+	};
+
+	// compile the template
+	var userDataHTML = userTemplate(userData);
 
 	// get a reference to the element in the DOM where we would like to display the data
 	var userDataElem = document.querySelector(".userData");
