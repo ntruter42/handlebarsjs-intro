fa4d14ccf89b05cef085e6d2d77bb4551b046697
ntruter42 <ntruter42@gmail.com>
2023-05-19 10:29:44 +0200

    Add regsiration_numbers/ - added registration_numbers widget to use handlebarsjs templates

---
+++ registration_numbers/js/registration_number.js
@@ -0,0 +1,129 @@
+function RegistrationNumber() {
+	let regNum = '';
+	let regList = JSON.parse(localStorage.getItem('regList')) || {};
+	let message = {};
+
+	const regTownList = {
+		'CA': 'Cape Town',
+		'CF': 'Kuils River',
+		'CG': 'Oudtshoorn',
+		'CJ': 'Paarl',
+		'CK': 'Malmesbury',
+		'CL': 'Stellenbosch'
+	};
+
+	function setReg(regNumValue) {
+		regNum = regNumValue.toUpperCase();
+	}
+
+	function getReg() {
+		return regNum;
+	}
+
+	function isValidReg() {
+		if (!/^C[AFGJKL]( |)(\d{3,6}|\d{1,5}(-| )\d{1,5})$/.test(regNum)) {
+			message = { 'Invalid registration number format': 'red' };
+			return false;
+		}
+		return true;
+	}
+
+	function isValidCode() {
+		if (!/C[AFGJKL]/.test(regNum.slice(0, 2))) {
+			message = { "Invalid registration number code": 'red' };
+			return false;
+		}
+		return true;
+	}
+
+	function getRegCode() {
+		return regNum.slice(0, 2);
+	}
+
+	function getRegTown(code) {
+		return regTownList[code];
+	}
+
+	function addToRegList() {
+		if (regList[regNum] === undefined) {
+			regList[regNum] = getRegCode();
+			localStorage.setItem('regList', JSON.stringify(regList));
+			return true;
+		}
+		return false;
+	}
+
+	function removeFromRegList(regNumValue) {
+		if (regList[regNumValue] !== undefined) {
+			delete regList[regNumValue];
+		}
+	}
+
+	function setRegList(regListValue) {
+		regList = regListValue;
+	}
+
+	function getRegList() {
+		return regList;
+	}
+
+	function clearRegList() {
+		regList = {};
+		localStorage.removeItem("regList");
+
+		// Uncomment and alter to delete individual entries
+		// delete regList['CJ 20025 9'];
+		// localStorage.setItem('regList', JSON.stringify(regList));
+	}
+
+	// TODO: add exceptions within individual functions
+	function addExceptionMessage() {
+		if (regNum === '') {
+			return { 'Enter a registration number': 'red' };
+		} else if (!isValidReg()) {
+			if (!isValidCode()) {
+				return { 'Registration code is invalid': 'red' };
+			} else {
+				return { 'Registration number format is invalid': 'red' };
+			}
+		} else if (!addToRegList()) {
+			return { 'Registration number already exists': 'orange' };
+		} else {
+			return { 'Registration number added succesfully': 'green' };
+		}
+	}
+
+	function clearExceptionMessage() {
+		if (!regNumList) {
+			return { 'Registration numbers NOT cleared': 'orange' };
+		} else {
+			return { 'Registration numbers cleared succesfully': 'green' };
+		}
+	}
+
+	function setMessage(messageValue, type) {
+		message = { messageValue: type };
+	}
+
+	function getMessage() {
+		return message;
+	}
+
+	return {
+		setReg,
+		getReg,
+		isValidReg,
+		isValidCode,
+		getRegCode,
+		getRegTown,
+		addToRegList,
+		removeFromRegList,
+		setRegList,
+		getRegList,
+		clearRegList,
+		addExceptionMessage,
+		clearExceptionMessage,
+		setMessage,
+		getMessage
+	};
+}
---
+++ registration_numbers/js/registration_number_dom.js
@@ -0,0 +1,178 @@
+// INPUT ELEMENTS
+const input = document.querySelector('#reg-input');
+const button = document.querySelector('#reg-button');
+const clear = document.querySelector('#reg-clear');
+const select = document.querySelector('#reg-filter');
+let option = select.options[select.selectedIndex];
+
+// OUTPUT ELEMENTS
+const regNumList = document.querySelector('#reg-num-container');
+const messageBox = document.querySelector('#message-container');
+const messageText = document.querySelector('#reg-message');
+const emptyBox = document.querySelector('#empty-container');
+const emptyText = document.querySelector('#reg-empty');
+
+// FUNTIONALITY
+let messageTimeout = 0;
+
+// INITIALISATION
+const reg = RegistrationNumber();
+showRegPlates(option.value);
+
+function showEmpty(code) {
+	if (!regNumList.firstElementChild && code !== '') {
+		emptyText.innerHTML = 'No registration numbers for<br>' + reg.getRegTown(code) + ' (' + code + ')';
+		emptyBox.classList.remove('hidden');
+		regNumList.style.resize = 'none';
+	} else if (!regNumList.firstElementChild && code === '') {
+		emptyText.innerHTML = 'No registration numbers';
+		emptyBox.classList.remove('hidden');
+		regNumList.style.resize = 'none';
+	} else {
+		emptyBox.classList.add('hidden');
+		regNumList.style.resize = 'horizontal';
+	}
+}
+
+function displayMessage(msgObj) {
+	clearTimeout(messageTimeout);
+
+	for (const message in msgObj) {
+		const color = msgObj[message];
+
+		messageBox.classList.remove('hidden', 'red', 'orange', 'green');
+		regNumList.classList.add('display-height-offset');
+
+		messageText.innerHTML = message;
+
+		switch (color) {
+			case 'red':
+				messageBox.classList.add('red');
+				break;
+			case 'orange':
+				messageBox.classList.add('orange');
+				break;
+			case 'green':
+				messageBox.classList.add('green');
+				break;
+			default:
+				break;
+		}
+
+		let duration = message.length * 100;
+
+		if (message === 'Registration code is invalid') {
+			messageTimeout = setTimeout(function () {
+				messageText.innerHTML = 'Valid registration codes: ';
+				messageText.innerHTML += '<b>CA</b>, <b>CF</b>, <b>CG</b>, <b>CJ</b>, <b>CK</b>, <b>CL</b>';
+				messageText.innerHTML += '';
+
+				messageBox.classList.remove('red', 'orange', 'green');
+				messageBox.classList.add('transparent');
+			}, duration);
+		} else if (message === 'Registration number format is invalid') {
+			messageTimeout = setTimeout(function () {
+				messageText.innerHTML = 'Valid format examples: ';
+				messageText.innerHTML += '<b>CA123456</b>, <b>CF 456 789</b>, <b>CG 789-012</b>, <b>CJ 345</b>';
+
+				messageBox.classList.remove('red', 'orange', 'green');
+				messageBox.classList.add('transparent');
+			}, duration);
+		} else {
+			messageTimeout = setTimeout(function () {
+				messageBox.classList.add('hidden');
+				regNumList.classList.remove('display-height-offset');
+			}, duration);
+		}
+	}
+}
+
+function addValidRegPlate() {
+	reg.setReg(input.value.toUpperCase());
+	displayMessage(reg.addExceptionMessage());
+	showRegPlates(option.value);
+	input.value = "";
+}
+
+function addRegPlate(regNumInput) {
+	if (regNumInput) {
+
+		const regNumItem = document.createElement('li');
+		if (regNumItem) {
+
+			const regNumPlate = document.createElement('div');
+			if (regNumPlate) {
+				regNumPlate.classList.add('plate');
+
+				const regNum = document.createElement('span');
+				if (regNum) {
+					regNum.classList.add('reg-num');
+					regNum.innerHTML = regNumInput.toUpperCase();
+
+					regNumPlate.appendChild(regNum);
+					regNumItem.appendChild(regNumPlate);
+					regNumList.appendChild(regNumItem);
+				}
+			}
+		}
+	}
+}
+
+function showRegPlates(filter) {
+	clearRegPlates();
+
+	for (const regNum of Object.keys(reg.getRegList())) {
+		if (regNum.startsWith(filter) || filter === "") {
+			addRegPlate(regNum);
+		}
+	}
+
+	option = select.options[select.selectedIndex];
+	showEmpty(option.value);
+}
+
+function clearRegPlates() {
+	const regPlates = document.querySelectorAll('li');
+
+	regPlates.forEach(plate => {
+		// TODO: remove only elements from filter location
+		plate.remove();
+	});
+}
+
+button.addEventListener('click', addValidRegPlate);
+
+input.addEventListener('keydown', function (event) {
+	if (event.keyCode === 13) {
+		event.preventDefault();
+		addValidRegPlate();
+	}
+});
+
+select.addEventListener('change', function () {
+	option = select.options[select.selectedIndex];
+	showRegPlates(option.value);
+});
+
+clear.addEventListener('click', function () {
+	if (confirm("Are you sure you want to clear all registration numbers?") === true) {
+		reg.clearRegList();
+		clearRegPlates();
+		showRegPlates(option.value);
+		displayMessage(reg.clearExceptionMessage());
+	}
+});
+
+// TODO: snap registration display window
+// function resize() {
+// 	console.log(regNumList.clientWidth);
+
+// 	if (regNumList.clientWidth < (330 + 660) / 2) {
+// 		regNumList.style.width = '330px';
+// 	} else if (regNumList.clientWidth < (660 + 990) / 2) {
+// 		regNumList.style.width = '660px';
+// 	} else {
+// 		regNumList.style.width = '990px';
+// 	}
+// }
+// window.addEventListener('resize', resize);
